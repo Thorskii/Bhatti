9c6717b2d7dda5d76065f945c10bd05cca8903c5
Thorskii <utahsmile05@hotmail.com>
2024-02-22 18:32:27 -0700

    added animations for hamburger menu and page transitions. Home page draft.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,46 @@
+"use client"
+import { motion } from "framer-motion";
+
 const AboutPage = () => {
     return (
-        <div className="">AboutPage</div>
+        <motion.div 
+        className="h-full" 
+        initial={{ y:"-200vh" }} 
+        animate={{ y:"0%" }} 
+        transition={{ duration:1 }}
+      >
+        {/* CONTAINER */}
+        <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 text-black dark:text-neutral-200">
+            {/* TEXT CONTAINER */}
+            <div className="">
+                {/* BIOGRAPHY CONTAINER */}
+                <div className="flex flex-col gap-12 justify-center">
+                    {/* BIOGRAPHY TITLE */}
+                    <h1>BIOGRAPHY</h1>
+                    {/* BIOGRAPHY DESC */}
+                    <p>
+                        Hello! My name is Gurpreet Bhatti, and I am a dedicated real estate agent with a passion for helping clients achieve their goals. With a diverse background and a commitment to excellence, I bring a unique perspective to the real estate industry.
+                        After retiring from the military and pursuing degrees in mechanical engineering and structural engineering, I embarked on a new adventure in real estate. Leveraging my experience running successful retail businesses, including smoke shops, gas stations, and warehouses, I developed a deep understanding of the market and a keen eye for high-margin investments.
+                        My expertise extends to various property types, from gas stations and strip plazas to hotels/motels, smoke shops, and commercial/residential lands. I excel at evaluating investment opportunities, analyzing cap rates, and ensuring optimal returns. Negotiation is a skill I pride myself on, always striving to secure the best outcomes for my clients.
+                        Beyond my professional life, I'm an avid lover of the outdoors. Whether it's camping, hunting, fishing, boating, running, or cycling, I find joy and inspiration in nature. In the summer, you'll often spot me cruising on my motorcycle, and during the winter, I'm out carving slopes on my snowboard. This personal passion for adventure allows me to connect with clients on a deeper level and understand their desires for properties that complement their active lifestyles.
+                        With a strong focus on delivering exceptional service, attention to detail, and utilizing my engineering background, I offer clients a well-rounded approach to real estate. I take pride in guiding my clients through every step of the process, ensuring a smooth and successful transaction.
+                        Outside of work, I value spending quality time with my loved ones and engaging in community initiatives. Giving back is important to me, and I actively seek ways to contribute positively to the community I serve.
+                        Whether you're a first-time buyer, a seasoned investor, or looking to sell your property, I'm here to provide expert guidance and personalized solutions. Let's work together, and with my expertise and dedication, we'll make your real estate dreams a reality!
+                    </p>
+                    {/* BIOGRAPHY QUOTE */}
+                    <span>Turning Dreams into Realty: Your Partner in Real Estate Excellence.</span>
+                    {/* BIOGRAPHY SIGN SVG*/}
+                    <div className=""></div>
+                </div>
+                {/* SKILLS CONTAINER */}
+                <div className="">SKILLS</div>
+                {/* EXPERIENCE CONTAINER */}
+                <div className="">EXPERIENCE</div>
+            </div>
+            {/* SVG CONTAINER */}
+            <div className="hidden"></div>
+        </div>
+    </motion.div>
     )
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import NavLink from "./navLink";
 import Image from "next/image";
 import { useState } from "react";
+import { motion } from "framer-motion"
 
 const links = [
     { url: "/", title: "Home" },
@@ -12,19 +13,67 @@ const links = [
 ];
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const topVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+    const centerVariants={
+        closed:{
+            opacity:1,
+        },
+        opened:{
+            opacity:0,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+    const bottomVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:-45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+    const listVariants={
+        closed:{
+            x:"100vw",
+        },
+        opened:{
+            x:0,
+            transition:{
+                when:"beforeChildren",
+                staggerChildren: 0.2,
+            }
+        }
+    }
+    const listItemVariants = {
+        closed:{
+            x:-10,
+            opacity:0,
+        },
+        opened:{
+            x:0,
+            opacity:1,
+        }
+    }
     return (
-        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-black dark:text-neutral-200">
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl text-black dark:text-neutral-200">
             {/* LINKS */}
             <div className="hidden md:flex gap-4 w-1/3">
                 {links.map(link=>(
                     <NavLink link={link} key={link.title}></NavLink>                ))}
             </div>
             {/* LOGO */}
-            <div className="md:hidden lg:flex w-1/3 justify-center">
-                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
-                    <span className="text-white mr-2">Bhatti</span>
-                    <span className="w-20 h-8 rounded bg-slate-300 text-black flex items-center justify-center">Real Estate</span>
+            <div className="md:hidden lg:flex w-1/3 xl:justify-center">
+                <Link href="/" className="sm:text-xl md:text-2xl lg:text-4xl p-1 font-semibold flex items-center justify-center">
+                    <span className="text-black dark:text-white mr-2">GURPREET BHATTI</span>
                 </Link>
             </div>
             {/* SOCIALS */}
@@ -35,21 +84,26 @@ const Navbar = () => {
                 <Link href="/">
                     <Image src="/facebook.png" alt="" width={24} height={24}></Image>
                 </Link>
+                <Link href="https://www.linkedin.com/in/gurpreet-bhatti-386291284/">
+                    <Image src="/linkedin.png" alt="" width={24} height={24}></Image>
+                </Link>
             </div>
             {/* HAMBURGER MENU */}
             <div className="md:hidden">
                 <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(()=>setOpen(!open))}>
-                    <div className="w-10 h-1 bg-black dark:bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-black dark:bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-black dark:bg-white rounded"></div>
+                    <motion.div variants={topVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-black dark:bg-white rounded origin-left"></motion.div>
+                    <motion.div variants={centerVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-black dark:bg-white rounded"></motion.div>
+                    <motion.div variants={bottomVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-black dark:bg-white rounded origin-left"></motion.div>
                 </button>
                 {/* MENU LIST */}
                 {open && (
-                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                    <motion.div variants={listVariants} initial="closed" animate="opened" className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
                     {links.map(link=>(
-                        <Link href={link.url} key={link.title}>{link.title}</Link>
+                        <motion.div variants={listItemVariants} className="" key={link.title}>
+                            <Link href={link.url}>{link.title}</Link>
+                        </motion.div>
                         ))}
-                    </div>
+                    </motion.div>
                 )}  
                 </div>
             </div>
